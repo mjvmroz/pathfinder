2260a2059a2d09b941e4bb99af54e5ae0665d021
Michael Mroz <mmroz@atlassian.com>
2016-03-05 03:17:46 +1100

    Fixed some broken indentation in AStarNode

--- src/AStarNode.java
+++ src/AStarNode.java
@@ -6,44 +6,44 @@ public class AStarNode extends Node
     /**
      * The anticipated distance from this node to the goal node
      */
-	private int h;
+    private int h;
 
     /**
      * Create a new A* Node with the supplied name and heuristic
      * @param name name
      * @param h heuristic
      */
-	public AStarNode(String name, int h)
-	{
+    public AStarNode(String name, int h)
+    {
         super(name);
-		setH(h);
-	}
+        setH(h);
+    }
 
     /**
      * Get the anticipated total distance to the goal if this node is chosen to pass through
      * @return
      */
     @Override
-	public int getF()
-	{
-		return getG() + getH();
-	}
+    public int getF()
+    {
+        return getG() + getH();
+    }
 
     /**
      * Get the current distance from the start node
      * @return
      */
-	public int getH()
-	{
-		return h;
-	}
+    public int getH()
+    {
+        return h;
+    }
 
     /**
      * Set the current distance from the start node
      * @param h
      */
-	public void setH(int h)
-	{
-		this.h = h;
-	}
+    public void setH(int h)
+    {
+        this.h = h;
+    }
 }
